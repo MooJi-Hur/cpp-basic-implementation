dd323f9ed430f1016299505045108edb8003fbf7
MooJi <mooji.hur@gmail.com>
2024-11-14 09:41:11 +0900

    [Feature] say hello

---
+++ problem-set/1001_Print_Output_01/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: 1001 Print_Output_01
+ * URL: https://codeup.kr/problem.php?id=1001
+ * Description: [Add description here if needed]
+ */
+
+// Solution code starts here
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  cout << "Hello";
+
+  return 0;
+}
